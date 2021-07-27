00c3de5dd4798f811bb925411a3614b5a42baade
Alex D <ad@hfour.com>
2021-07-27 18:39:45 +0400

    auth0 react exampple

--- auth0/src/App.js
+++ auth0/src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
+import LoginButton from './components/LoginButton'
+import LogoutButton from './components/LogoutButton'
+import Profile from "./components/Profile";
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <>
+    <LoginButton />
+    <LogoutButton />
+    <Profile />
+    </>
   );
 }
 
